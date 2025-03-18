97de4a786ef05a4472ba6826ba052b640d76839d
ft-nion <a5demayo343@hotmail.com>
2025-03-18 11:27:52 -0600

    modificar dashboard

--- dashboard.php
+++ dashboard.php
@@ -13,6 +13,8 @@ if (!isset($_SESSION['loggedin'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/dashboard.css">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/recipes.css">
 </head>
 <body>
     <header>
